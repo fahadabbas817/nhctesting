d40766a31f52b3185d59e713815e58b140943310
Syed Fahad Abbas <110738135+fahadabbas817@users.noreply.github.com>
2025-02-05 12:49:48 +0500

    bankai

--- src/streamingAvatarComponent/use-streaming-avatar.tsx
+++ src/streamingAvatarComponent/use-streaming-avatar.tsx
@@ -82,7 +82,7 @@ export default function useStreamingAvatar(): UseStreamingAvatar {
 
     const handleStart = useCallback(async () => {
         console.log('handlestart started loading')
-        if (isConnectionOpen || isStarted || session) return;
+        // if (isConnectionOpen || isStarted || session) return;
         console.log('ethy nhi ho payega')
         setIsLoadingNewSession(true);
         createNewSession(
@@ -142,8 +142,19 @@ export default function useStreamingAvatar(): UseStreamingAvatar {
                         }, total)
                     }
                 });
-        } catch (e) {
+        } catch (e:any) {
             console.log('handleRepeat Error', e)
+            // Check if the error is a 400 Bad Request
+        if (e.response?.status === 400) {
+            console.error('Bad Request detected. Reloading page...');
+            
+            // Optionally show an error message to the user
+            alert('An error occurred (400 Bad Request). The page will reload.');
+
+            // Reload the page
+            window.location.reload();
+        }
+            
         } finally {
 
         }
